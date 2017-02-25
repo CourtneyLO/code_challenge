12f5280bc35f7f581581a2b7d052b3190728be75
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-02-25 13:32:06 +0000

    Adds model data to server

--- KompasApp/routes/users.js
+++ KompasApp/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Users');
-});
+router.route('/')
+    .get(function(req, res, next) {
+        mongoose.model('User').find({}, function (err, users) {
+              if (err) {
+                  return console.error(err);
+              } else {
+                  res.json(users);
+              }
+        });
+    })
+
 
 module.exports = router;
